50043b666cea567b4ab6f68dea2c6d3be649d8ae
Milumon <17993682+Milumon@users.noreply.github.com>
2021-04-26 02:30:06 -0500

    change ImageView to ImageButton

---
+++ app/src/main/java/com/isil/restaurant/SplashActivity.java
@@ -0,0 +1,26 @@
+package com.isil.restaurant;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        Handler handler = new Handler(Looper.getMainLooper());
+        handler.postDelayed(() -> {
+            mostrarMain();
+        }, 3000);
+    }
+
+    private void mostrarMain() {
+        startActivity(new Intent(this, IntroPageActivity.class));
+        finish();
+    }
+}
